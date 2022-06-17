579ad4ad6d45115d7c0f8dd13f7decca4281b25c
KateChu <chu.shihchien@gmail.com>
2022-06-17 11:12:31 +0800

    upload trace

---
+++ trace.js
@@ -0,0 +1,33 @@
+const traceList = []
+
+const newTrace = [
+  {
+    name: '彈性棉質混紡圓裙',
+    url: 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000011967'
+  },
+  {
+    name: '綁帶涼鞋',
+    url: 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000012816'
+  },
+  {
+    name: '嫘縈印花上衣(短袖)',
+    url: 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000012572'
+  },
+  {
+    name: 'U AIRism棉質寬版圓領T恤(短袖)',
+    url: 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000000167'
+  },
+  {
+    name: 'DRY-EX 短版圓領T恤 (短袖)',
+    url: 'https://www.uniqlo.com/tw/zh_TW/product-detail.html?productCode=u0000000000230'
+  }
+]
+
+const deleteTrace = [
+]
+
+module.exports = {
+  traceList,
+  newTrace,
+  deleteTrace
+}
